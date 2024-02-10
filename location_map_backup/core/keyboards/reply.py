37f6bb94a0c9187cd19033a75338a3fcb1c1df25
exclus1veble <159007138+exclus1veble@users.noreply.github.com>
2024-02-10 10:31:59 +0200

    Delete location_map_backup directory

--- location_map_backup/core/keyboards/reply.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-reply_keyboard = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Карта')],
-    [KeyboardButton(text='Добавить событие')]
-], resize_keyboard=True, one_time_keyboard=True)
